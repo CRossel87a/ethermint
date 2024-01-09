fedc03811249143a8fe0516e43f26f68ed557202
CRossel87a <chris.rossel@gmail.com>
2024-01-09 11:59:48 +0100

    Update utils.go

--- client/keys/utils.go
+++ client/keys/utils.go
@@ -3,11 +3,9 @@ package keys
 import (
 	"fmt"
 	"io"
-	"path/filepath"
 
 	yaml "gopkg.in/yaml.v2"
 
-	"github.com/cosmos/cosmos-sdk/client/keys"
 	cryptokeyring "github.com/cosmos/cosmos-sdk/crypto/keyring"
 )
 
@@ -20,37 +18,8 @@ const (
 	defaultKeyDBName = "keys"
 )
 
-type bechKeyOutFn func(keyInfo cryptokeyring.Info) (cryptokeyring.KeyOutput, error)
-
 // NewLegacyKeyBaseFromDir initializes a legacy keybase at the rootDir directory. Keybase
 // options can be applied when generating this new Keybase.
-func NewLegacyKeyBaseFromDir(rootDir string, opts ...cryptokeyring.KeybaseOption) (cryptokeyring.LegacyKeybase, error) {
-	return getLegacyKeyBaseFromDir(rootDir, opts...)
-}
-
-func getLegacyKeyBaseFromDir(rootDir string, opts ...cryptokeyring.KeybaseOption) (cryptokeyring.LegacyKeybase, error) {
-	return cryptokeyring.NewLegacy(defaultKeyDBName, filepath.Join(rootDir, "keys"), opts...)
-}
-
-func printKeyInfo(w io.Writer, keyInfo cryptokeyring.Info, bechKeyOut bechKeyOutFn, output string) {
-	ko, err := bechKeyOut(keyInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	switch output {
-	case OutputFormatText:
-		printTextInfos(w, []cryptokeyring.KeyOutput{ko})
-
-	case OutputFormatJSON:
-		out, err := keys.KeysCdc.MarshalJSON(ko)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Fprintln(w, string(out))
-	}
-}
 
 func printTextInfos(w io.Writer, kos []cryptokeyring.KeyOutput) {
 	out, err := yaml.Marshal(&kos)
